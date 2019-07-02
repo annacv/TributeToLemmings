52919de70c6cf5b60a8863bb7faacb472697ca99
annacv <21217131+annacv@users.noreply.github.com>
2019-07-02 22:24:31 +0200

    [Player] Create object and drawImage method

--- assets/js/Lemming.js
+++ assets/js/Lemming.js
@@ -1,1 +1,28 @@
 'use strict'
+
+/*Player Object */
+
+function Player(canvas) {
+  this.canvas = canvas;
+  this.ctx = canvas.getContext('2d');
+  this.image = new Image();
+  this.image.src = "./assets/images/svg/lemming.svg";
+  this.sx = 0;
+  this.sy = 0;
+  this.sWidth = 142;
+  this.sHeight = 142;
+  this.dx = 40;
+  this.dy = 380;
+  this.dWidth = 50;
+  this.dHeight = 50;
+  this.lives = 3;
+  this.direction = 0;
+}
+
+
+/* Methods */
+
+// drawImage
+Player.prototype.drawImage = function() {
+  this.ctx.drawImage(this.image, this.sx, this.sy, this.sWidth, this.sHeight, this.dx, this.dy, this.dWidth, this.dHeight);
+}
