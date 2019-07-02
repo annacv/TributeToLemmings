92e155325b840ece71bada4504d54f88879568cf
annacv <21217131+annacv@users.noreply.github.com>
2019-07-02 22:22:49 +0200

    [Loop] Add lop w/draw method call

--- assets/js/Game.js
+++ assets/js/Game.js
@@ -1,6 +1,7 @@
 'use strict'
 
 function Game(canvas) {
+  this.player = null;
   this.isGameOver = false;
   this.canvas = canvas;
   this.ctx = this.canvas.getContext('2d');
@@ -8,7 +9,22 @@ function Game(canvas) {
 }
 
 Game.prototype.startGame = function() {
-  console.log('aloha');
+  this.player = new Player(this.canvas);
+
+  var loop = () => {
+    this.draw();
+    if (!this.isGameOver) {
+      requestAnimationFrame(loop);
+    } else {
+      this.onGameOver();
+    }
+  };
+  loop();
+}
+
+Game.prototype.draw = function() {
+  console.log('draw is called');
+  this.player.drawImage();
 }
 
 Game.prototype.gameOverCallback = function(callback) {
