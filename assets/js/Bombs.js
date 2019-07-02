6df095487de5db032a3d816d38778c71dff3493a
annacv <21217131+annacv@users.noreply.github.com>
2019-07-02 23:41:57 +0200

    [Bomb] create object and drawImage method

--- assets/js/Bombs.js
+++ assets/js/Bombs.js
@@ -1,1 +1,28 @@
 'use strict'
+
+/*Player Object */
+
+function Bomb(canvas) {
+  this.canvas = canvas;
+  this.ctx = canvas.getContext('2d');
+  this.image = new Image();
+  this.image.src = "./assets/images/svg/bomb.svg";
+  this.sx = 0;
+  this.sy = 0;
+  this.sWidth = 76;
+  this.sHeight = 84;
+  this.dx = 0;
+  this.dy = 0;
+  this.dWidth = 38;
+  this.dHeight = 42;
+  this.speed = 3;
+  this.direction = 0;
+}
+
+
+/* Methods */
+
+// drawImage
+Bomb.prototype.drawImage = function() {
+  this.ctx.drawImage(this.image, this.sx, this.sy, this.sWidth, this.sHeight, this.dx, this.dy, this.dWidth, this.dHeight);
+}
