a044ab4a46606d5a12fd161380d9051e9a63a959
annacv <21217131+annacv@users.noreply.github.com>
2019-07-02 23:42:59 +0200

    [Game] Add bomb in  Game draw/canvas f()

--- assets/js/Game.js
+++ assets/js/Game.js
@@ -2,6 +2,7 @@
 
 function Game(canvas) {
   this.player = null;
+  this.bombs = [];
   this.isGameOver = false;
   this.canvas = canvas;
   this.ctx = this.canvas.getContext('2d');
@@ -12,6 +13,11 @@ Game.prototype.startGame = function() {
   this.player = new Player(this.canvas);
 
   var loop = () => {
+    if (Math.random() > 0.97) {
+      var newBomb = new Bomb(this.canvas);
+      this.bombs.push(newBomb);
+    }
+
     this.draw();
     if (!this.isGameOver) {
       requestAnimationFrame(loop);
@@ -25,6 +31,9 @@ Game.prototype.startGame = function() {
 Game.prototype.draw = function() {
   console.log('draw is called');
   this.player.drawImage();
+  this.bombs.forEach(function(bomb) {
+    bomb.drawImage();
+  })
 }
 
 Game.prototype.gameOverCallback = function(callback) {
